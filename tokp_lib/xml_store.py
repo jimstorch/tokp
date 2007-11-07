89a0c7da1eb58de2de663a18a1834c4d7a7409ad
jimstorch <jimstorch@f3c4b8af-f63d-0410-8965-49ce8565d843>
2007-11-07 19:06:12 +0000

    working on XML based storage

---
+++ tokp_lib/xml_store.py
@@ -0,0 +1,63 @@
+#------------------------------------------------------------------------------
+#   File:       xml_store.py
+#   Purpose:    Store and Retrieve data from XML files
+#   Author:     Jim Storch
+#   Revised:
+#   License:    GPLv3 see LICENSE.TXT    
+#------------------------------------------------------------------------------
+
+import datetime
+import re
+from xml.etree import cElementTree as et
+
+
+rawstr = r"^(?P<year>\d{2,})-(?P<month>\d\d)-(?P<day>\d\d)\s(?P<hour>\d\d)" + \
+    ":(?P<minute>\d\d):(?P<second>\d\d)\.(?P<micro>\d*)$"
+compile_obj = re.compile(rawstr)
+
+
+#--[ Converting Datetime Objects ]---------------------------------------------
+
+## Seems like a lot of juggling but strftime() and strptime() don't support
+## microseconds.
+
+def dt_to_str(dt):
+    ## Convert a datetime to a string formated YYYY-MM-DD HH:MM:SS:MMMMMM
+    return '%d-%.2d-%.2d %.2d:%.2d:%.2d.%.6d' % (
+         dt.year, dt.month, dt.day,
+         dt.hour, dt.minute, dt.second, dt.microsecond )
+
+def str_to_dt(string):
+    ## Convert a string back into datetime object
+    match_obj = compile_obj.search(string)
+    if match_obj:
+        year = int(match_obj.group('year'))
+        month = int(match_obj.group('month'))
+        day = int(match_obj.group('day'))
+        hour = int(match_obj.group('hour'))
+        minute = int(match_obj.group('minute'))
+        second = int(match_obj.group('second'))
+        micro = int(match_obj.group('micro'))
+        return datetime.datetime(year, month, day, hour, minute, second, micro)
+    
+    else:
+        raise ValueError('Could not parse datetime string')    
+
+
+#--[ Raid to XML ]-------------------------------------------------------------
+
+def raid_to_xml(raid):
+    
+    xml = et.Element('raid', zone = raid.zone, 
+        start_time = dt_to_str(raid.start_time),
+        end_time = dt_to_str(raid.end_time))
+            
+    for member in raid.raid_members:
+        guy = et.Element('member',name=member)
+        xml.append(guy)  
+
+    return et.tostring(xml)
+
+    
+
+
---
+++ xml_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#------------------------------------------------------------------------------
+#   File:       xml_test.py
+#   Purpose:    
+#   Author:     Jim Storch
+#   Revised:
+#   License:    GPLv3 see LICENSE.TXT    
+#------------------------------------------------------------------------------
+
+import datetime
+
+from tokp_lib.parse_combat import Raid 
+from tokp_lib.xml_store import raid_to_xml
+
+from tokp_lib.xml_store import dt_to_str, str_to_dt
+
+
+timestamp = datetime.datetime.now()
+ten_minutes = datetime.timedelta(minutes=10)
+
+raid = Raid('Test Zone', timestamp)
+for member in ['Redguy','Greenguy','Blueguy','Orangeguy','Greyguy','Brownguy']:
+    timestamp += ten_minutes
+    raid.add_member(member, timestamp)
+raid.end_time = timestamp + ten_minutes    
+
+xml = raid_to_xml(raid)
+
+print xml
